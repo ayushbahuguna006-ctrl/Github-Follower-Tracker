74ec43d2bd12babb691a302b4e2d51b558267bb6
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2025-12-24 23:06:48 +0530

    Project Comepleted

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Dashboard() {
-
+const [user,setuser]=useState()
+const [followers,setfolllowers]=useState()
+const [loading, setLoading] = useState(true);
+const github=async ()=>{
+const res=await fetch (`https://api.github.com/users/${user}`)
+const data=await res.json()
+console.log(data)
+setfolllowers(data.followers)
+setLoading(false)
+}
 
 
 
@@ -14,15 +23,15 @@ function Dashboard() {
     <div className='min-h-screen  flex flex-col items-center justify-center py-10 bg-blue-50'>
           <div className="w-[90%] md:w-[32%] h-50 bg-white/90 backdrop-blur-md border border-gray-200 rounded-2xl shadow-xl hover:shadow-2xl transition-all duration-300"
 >
-                   <div className='flex flex-col items-center'><label><h1 className='font-medium text-xl text-black text-center pt-10'>Enter your Github username</h1><div className='text-center border-purple-200 border mt-3 rounded-lg h-7'><input type="text" required   placeholder="                         Here!" className='w-full h-full'/></div><div className='text-center pt-8'><button className='bg-blue-200 p-2 w-1/2 rounded-xl hover:bg-green-200  '>GET</button></div></label></div>
+                   <div className='flex flex-col items-center'><label><h1 className='font-medium text-xl text-black text-center pt-10'>Enter your Github username</h1><div className='text-center border-purple-200 border-2 mt-3 rounded-lg h-7'><input type="text" required value={user} onChange={(e)=>setuser(e.target.value)}   placeholder="                      Here!" className='w-full h-full rounded-lg outline-none px-2'/></div><div className='text-center pt-8'><button onClick={github} className='bg-blue-200 p-2 w-1/2 rounded-xl hover:bg-green-200 cursor-pointer  '>GET</button></div></label></div>
 
 
 
           </div>
-           <div id='ani1' className="w-[90%] md:w-[32%] h-50 bg-white/90 backdrop-blur-md border border-gray-200 rounded-2xl shadow-xl hover:shadow-2xl transition-all duration-300">
+           <div id='ani1' className="w-[90%] md:w-[32%] h-50 bg-white/90 backdrop-blur-md border border-gray-200 rounded-2xl shadow-xl hover:shadow-2xl transition-all duration-300 flex justify-center items-center">
            
            
-           
+             {loading?(<p className='font-bold text-xl'>React is Fun  !!!</p>):(<p>{`${user} has ${followers} followers`}</p>)}
            
            
            </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function Navbar() {
   return (
     <div className='flex md:justify-evenly justify-between bg-gradient-to-r from-blue-200 to-purple-200 h-10 items-center text-black font-mono'>
                    
-         <div className='px-2'><NavLink to="/home" className={({isActive})=> ` p-1 border-black bg-gradient-to-r from-blue-200 to-red-50 border-2 font-bold  ${isActive?"text-blue-500":"text-black"}`}>HOME</NavLink></div>
-          <div><NavLink to="/dashboard" className={({isActive})=> ` p-1 bg-gradient-to-r from-blue-200 to-red-50 font-bold border-black border-2 ${isActive?"text-blue-500":"text-black"}`}>DASHBOARD</NavLink></div>
-         <div className='px-2'><NavLink to="/about" className={({isActive})=> ` p-1 border-black border-2 bg-gradient-to-r from-blue-200 to-red-50 font-bold ${isActive?"text-blue-500":"text-black"}`}>ABOUT</NavLink></div>
+         <div className='px-2'><NavLink to="/home" className={({isActive})=> ` p-1 border-black bg-gradient-to-r from-blue-200 to-purple-200 to-red-50 border-2 font-bold  ${isActive?"text-blue-500":"text-black"}`}>HOME</NavLink></div>
+          <div><NavLink to="/dashboard" className={({isActive})=> ` p-1 bg-gradient-to-r from-blue-200 to-purple-200 font-bold border-black border-2 ${isActive?"text-blue-500":"text-black"}`}>DASHBOARD</NavLink></div>
+         <div className='px-2'><NavLink to="/about" className={({isActive})=> ` p-1 border-black border-2 bg-gradient-to-r from-blue-200 to-purple-200 font-bold ${isActive?"text-blue-500":"text-black"}`}>ABOUT</NavLink></div>
         
           
              
