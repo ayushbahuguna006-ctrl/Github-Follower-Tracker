ef03b689fcce5c9bc3c030f07775bcbf96429d20
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2025-12-24 19:35:26 +0530

    UI compeleted

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,28 +2,7 @@ import React from 'react'
 
 function Dashboard() {
   return (
-      <div className="min-h-screen bg-gray-50 p-6">
-      <h1 className="text-4xl font-bold text-green-700 mb-6 text-center">
-        Dashboard
-      </h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div className="p-6 bg-white border rounded shadow hover:shadow-lg transition">
-          <h2 className="text-xl font-semibold mb-2">Users</h2>
-          <p className="text-gray-600">Track the number of active users and new sign-ups.</p>
-        </div>
-
-        <div className="p-6 bg-white border rounded shadow hover:shadow-lg transition">
-          <h2 className="text-xl font-semibold mb-2">Revenue</h2>
-          <p className="text-gray-600">Monitor the latest revenue and transactions.</p>
-        </div>
-
-        <div className="p-6 bg-white border rounded shadow hover:shadow-lg transition">
-          <h2 className="text-xl font-semibold mb-2">Reports</h2>
-          <p className="text-gray-600">Generate insights and export reports quickly.</p>
-        </div>
-      </div>
-    </div>
+     <h1>das</h1>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,12 @@ import Dashboard from './Dashboard';
 
 function Navbar() {
   return (
-    <div className='flex md:justify-evenly justify-between bg-white h-10 items-center text-black font-mono'>
+    <div className='flex md:justify-evenly justify-between bg-gradient-to-r from-blue-200 to-purple-200 h-10 items-center text-black font-mono'>
                    
-         <div className='px-2'><NavLink to="/home" className={({isActive})=> `hover:text-white hover:bg-black p-1 border-black border-2 ${isActive?"text-orange-500":"text-black"}`}>HOME</NavLink></div>
-         <div ><NavLink to="/about" className={({isActive})=> `hover:text-white hover:bg-black p-1 border-black border-2 ${isActive?"text-orange-500":"text-black"}`}>About</NavLink></div>
-         <div className='px-2'><NavLink to="/dashboard" className={({isActive})=> `hover:text-white hover:bg-black p-1 border-black border-2 ${isActive?"text-orange-500":"text-black"}`}>Dashboard</NavLink></div>
+         <div className='px-2'><NavLink to="/home" className={({isActive})=> ` p-1 border-black hover:bg-gray-100 border-2 font-bold  ${isActive?"text-orange-500":"text-black"}`}>HOME</NavLink></div>
+          <div><NavLink to="/dashboard" className={({isActive})=> ` p-1 hover:bg-gray-100 font-bold border-black border-2 ${isActive?"text-orange-500":"text-black"}`}>DASHBOARD</NavLink></div>
+         <div className='px-2'><NavLink to="/about" className={({isActive})=> ` p-1 border-black border-2 hover:bg-gray-100 font-bold ${isActive?"text-orange-500":"text-black"}`}>ABOUT</NavLink></div>
+        
           
              
     </div>
