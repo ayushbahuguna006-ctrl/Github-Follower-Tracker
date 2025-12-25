ab03bef77babc253945303424b14ff9bbbecfa09
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2025-12-25 13:52:51 +0530

    controlled API fetching

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,14 +4,15 @@ function Dashboard() {
 const [user,setuser]=useState()
 const [followers,setfolllowers]=useState()
 const [loading, setLoading] = useState(true);
-const [data, setData] = useState(null);
+const [data, setData] = useState({});
 const github=async ()=>{
+setLoading(true)
 const res=await fetch (`https://api.github.com/users/${user}`)
 const fdata=await res.json()
-console.log(data)
 setData(fdata)
-setfolllowers(data.followers)
+setfolllowers(fdata.followers)
 setLoading(false)
+console.log(fdata)
 }
 
 
@@ -25,14 +26,14 @@ setLoading(false)
     <div className='min-h-screen  flex flex-col items-center justify-center py-10 bg-blue-50'>
           <div className="w-[90%] md:w-[32%] h-50 bg-white/90 backdrop-blur-md border border-gray-200 rounded-2xl shadow-xl hover:shadow-2xl transition-all duration-300"
 >
-                   <div className='flex flex-col items-center'><label><h1 className='font-medium text-xl text-black text-center pt-10'>Enter your Github username</h1><div className='text-center border-purple-200 border-2 mt-3 rounded-lg h-7'><input type="text" required value={user} onChange={(e)=>setuser(e.target.value)}   placeholder="                      Here!" className='w-full h-full rounded-lg outline-none px-2'/></div><div className='text-center pt-8'><button onClick={github} className='bg-blue-200 p-2 w-1/2 rounded-xl hover:bg-green-200 cursor-pointer  '>GET</button></div></label></div>
+                   <div className='flex flex-col items-center'><label><h1 className='font-bold text-xl text-stone-900  text-center pt-10'>Enter your Github username</h1><div className='text-center border-purple-200 border-2 mt-3 rounded-lg h-10'><input type="text" required value={user} onChange={(e)=>setuser(e.target.value)}   placeholder="                      Here!" className='w-full h-full rounded-lg outline-none px-2'/></div><div className='text-center pt-8'><button onClick={github} className='bg-gradient-to-br from-slate-100 via-purple-300 to-slate-100 p-2 w-1/2 rounded-xl hover:bg-green-200 cursor-pointer  '>GET</button></div></label></div>
 
 
 
           </div>
            <div id='ani1' className="w-[90%] md:w-[32%] h-50 bg-white/90 backdrop-blur-md border border-gray-200 rounded-2xl shadow-xl hover:shadow-2xl transition-all duration-300 flex justify-center items-center">
            
-            {data?.message === "Not Found" ?(<p>Enter a Valid Username</p>):loading?(<p className='font-bold text-xl'>React is Fun  !!!</p>):(<p>{`${user} has ${followers} followers`}</p>)}
+            {data.message === "Not Found" ?(<p className='text-base text-red-500 font-bold'>Enter a Valid Username</p>):loading?(<p className=' p-1 border-y-red-200 border text-purple-700 border-x-purple-200 bg-blue-50'>REACT IS FUN </p>):(<p className='p-2 border-y-red-200 border-2 text-purple-500'>{`${user} has ${followers} followers`}</p>)}
             
            
            
